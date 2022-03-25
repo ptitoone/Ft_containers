37285ecdbce3613ed892d71fc559d569c3a26bae
Anthony Kotzky <akotzky@c3r2p5.42nice.fr>
2022-03-25 10:11:54 +0100

    Quick Push

--- main.cpp
+++ main.cpp
@@ -1,10 +1,47 @@
 #include "vector.hpp"
 
+template <class T>
+void	print_vector(ft::vector<T> &test)
+{
+	typename ft::vector<T>::iterator		beg = test.begin();
+	typename ft::vector<T>::iterator		end = test.end();
+	std::cout << "size : " << test.size() << ", capacity : " << test.capacity() << std::endl;
+	for (typename ft::vector<T>::iterator it = beg; it != end; it++)
+	{
+		std::cout << *it << " ";
+		if (((it - beg) % 10 == 9) && it > beg)
+			std::cout << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+template <class T>
+void	insert_tests()
+{
+	std::cout << std::endl << "INSERT TESTS" << std::endl;
+	ft::vector<T> test(1, 1);
+	ft::vector<T> test2(5, 5);
+
+	test.insert(test.begin(), 200, 12);
+	print_vector<T>(test);
+	test.insert(test.begin() + 12, 200, 30);
+	print_vector<T>(test);
+	test.insert(test.end(), 12, 50);
+	print_vector<T>(test);
+//	test.insert(test.end() - 1, 0, 60);
+//	print_vector<T>(test);
+//	test.insert(test.end() - 1, 1, 70);
+//	print_vector<T>(test);
+//	test.insert(test.begin() + 412, test2.begin(), test2.end());
+//	print_vector<T>(test);
+//	test.insert(test.begin() + 6, test2.begin(), test2.end());
+//	print_vector<T>(test);
+//	test.insert(test.end(), test2.begin(), test2.end());
+//	print_vector<T>(test);
+}
+
 int main()
 {
-    ft::vector<std::string> vec1(10, "Hello");
-
-	vec1.assign(42, "James BOND");
-
+    insert_tests<int>();
     return 0;
 }
